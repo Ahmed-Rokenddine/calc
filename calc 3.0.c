476943fa373863a1414a13c69a258106a8ac26b9
Ahmed-Rokenddine <masteriko005@gmail.com>
2022-09-09 16:39:13 +0100

    fixed some problemes om equation calc

--- calc 3.0.c
+++ calc 3.0.c
@@ -1,6 +1,5 @@
+
 #include <stdio.h>
-#include <conio.h>
-#include <stdbool.h>
 #include <math.h>
 
 
@@ -14,16 +13,17 @@ int main() {
     printf("c:");
     scanf("%d",&c);
     float x,delta,x1,x2;
-    delta=pow(a,2)-4*a*c;
+    delta=pow(b,2)-4*a*c;
     if (delta < 0)
     {
        printf("cette equation n'as pas de solution!");
     }
-    else if (delta = 0)
+    else if (delta == 0)
     {
         printf("cette equation a une seule solution : %f",x=-b/2*a);
     }
-    else printf("cette equation a deux solution : x1=\n x2=\n",x1=-b-sqrt(delta)/2*a,x2=-b+sqrt(delta)/2*a);
+    else if (delta> 0)
+    printf("cette equation a deux solution : x1=\n x2=\n",x1=-b-sqrt(delta)/2*a,x2=-b+sqrt(delta)/2*a);
 }
     
 
